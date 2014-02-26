ccda2d0e0afd3446bdac881beec89b553d2d87db
Mario Abruzzi <abruzzim@framework-solutions.com>
2014-02-26 16:54:24 -0500

    Base WSZMD Rails 4.0.3 Application Framework

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Wszmd::Application.config.secret_key_base = 'c1d9bb053538aa286aff5f82c7a343de77819d87530912cc1ce4b70aca6c5169e6b1a7e10e008760b3db0614eb398161a662fbc9a897247eaeed3f8ebe7d4abc'
